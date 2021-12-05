4c3eb464620f1433f7422f08b2d0750346346b74
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2021-12-05 22:04:20 +0100

    Add sort

---
+++ include/scilib/mdarray_impl/sort.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2021 Stig Rune Sellevag
+//
+// This file is distributed under the MIT License. See the accompanying file
+// LICENSE.txt or http://www.opensource.org/licenses/mit-license.php for terms
+// and conditions.
+
+#ifndef SCILIB_MDARRAY_SORT_H
+#define SCILIB_MDARRAY_SORT_H
+
+#ifdef USE_MKL
+#include <mkl.h>
+#else
+#include <lapacke.h>
+#endif
+
+#include <experimental/mdspan>
+#include <scilib/linalg_impl/lapack_types.h>
+#include <exception>
+
+namespace Scilib {
+namespace stdex = std::experimental;
+
+template <stdex::extents<>::size_type ext, class Layout, class Accessor>
+inline void sort(stdex::mdspan<double, stdex::extents<ext>, Layout, Accessor> x,
+                 char id = 'I')
+{
+    const BLAS_INT n = x.extent(0);
+
+    BLAS_INT info = LAPACKE_dlasrt(id, n, x.data());
+    if (info != 0) {
+        throw std::runtime_error("bad input to dlasrt");
+    }
+}
+
+} // namespace Scilib
+
+#endif // SCILIB_MDARRAY_SORT_H
