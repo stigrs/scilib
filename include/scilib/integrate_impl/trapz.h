586a2c76d07f2c493b6b78fef77881692986ee49
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2021-12-06 20:28:47 +0100

    Made trapz templated

--- include/scilib/integrate_impl/trapz.h
+++ include/scilib/integrate_impl/trapz.h
@@ -7,14 +7,37 @@
 #ifndef SCILIB_INTEGRATE_IMPL_TRAPZ_H
 #define SCILIB_INTEGRATE_IMPL_TRAPZ_H
 
-#include <scilib/mdarray.h>
+#include <experimental/mdspan>
+#include <type_traits>
+#include <cmath>
 
 namespace Scilib {
 namespace Integrate {
 
+namespace stdex = std::experimental;
+
 // Integrate function values over a non-uniform grid using the
 // trapezoidal rule.
-double trapz(double xlo, double xup, Scilib::Vector_view<double> y);
+// clang-format off
+template <class T,
+          stdex::extents<>::size_type ext,
+          class Layout,
+          class Accessor>
+    requires std::is_floating_point_v<T> 
+inline T
+trapz(T xlo, T xup, stdex::mdspan<T, stdex::extents<ext>, Layout, Accessor> x)
+// clang-format on
+{
+    using size_type = stdex::extents<>::size_type;
+
+    const T step = std::abs(xup - xlo) / (x.extent(0) - 1);
+    T ans = T{0};
+
+    for (size_type i = 1; i < x.extent(0); ++i) {
+        ans += 0.5 * (x(i) + x(i - 1));
+    }
+    return ans *= step;
+}
 
 } // namespace Integrate
 } // namespace Scilib
