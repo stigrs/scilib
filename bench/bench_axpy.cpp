37150450a5b78ca8ea0bf4f9b2aea293ddf4f532
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2021-12-08 09:35:40 +0100

    Cosmetic change

--- bench/bench_axpy.cpp
+++ bench/bench_axpy.cpp
@@ -61,7 +61,7 @@ void benchmark(int n)
 
     t1 = std::chrono::high_resolution_clock::now();
     for (int it = 0; it < 10; ++it) {
-        Scilib::Linalg::axpy(2.0, va.view(), vb.view());
+        axpy(2.0, va.view(), vb.view());
     }
     t2 = std::chrono::high_resolution_clock::now();
     Timer t_axpy = t2 - t1;
