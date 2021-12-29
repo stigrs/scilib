aed7123906788678bc2a039ca5ed972bb3373f30
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2021-12-29 14:20:41 +0100

    Add copy up to 7D array

--- include/scilib/mdarray_impl/copy.h
+++ include/scilib/mdarray_impl/copy.h
@@ -125,6 +125,160 @@ copy(stdex::
     }
 }
 
+template <class T,
+          stdex::extents<>::size_type n1_x,
+          stdex::extents<>::size_type n2_x,
+          stdex::extents<>::size_type n3_x,
+          stdex::extents<>::size_type n4_x,
+          stdex::extents<>::size_type n5_x,
+          class Layout_x,
+          class Accessor_x,
+          stdex::extents<>::size_type n1_y,
+          stdex::extents<>::size_type n2_y,
+          stdex::extents<>::size_type n3_y,
+          stdex::extents<>::size_type n4_y,
+          stdex::extents<>::size_type n5_y,
+          class Layout_y,
+          class Accessor_y>
+inline void copy(stdex::mdspan<T,
+                               stdex::extents<n1_x, n2_x, n3_x, n4_x, n5_x>,
+                               Layout_x,
+                               Accessor_x> x,
+                 stdex::mdspan<T,
+                               stdex::extents<n1_y, n2_y, n3_y, n4_y, n5_x>,
+                               Layout_y,
+                               Accessor_y> y)
+{
+    static_assert(x.static_extent(0) == y.static_extent(0));
+    static_assert(x.static_extent(1) == y.static_extent(1));
+    static_assert(x.static_extent(2) == y.static_extent(2));
+    static_assert(x.static_extent(3) == y.static_extent(3));
+    static_assert(x.static_extent(4) == y.static_extent(4));
+
+    using size_type = stdex::extents<>::size_type;
+
+    for (size_type i1 = 0; i1 < y.extent(0); ++i1) {
+        for (size_type i2 = 0; i2 < y.extent(1); ++i2) {
+            for (size_type i3 = 0; i3 < y.extent(2); ++i3) {
+                for (size_type i4 = 0; i4 < y.extent(3); ++i4) {
+                    for (size_type i5 = 0; i5 < y.extent(4); ++i5) {
+                        y(i1, i2, i3, i4, i5) = x(i1, i2, i3, i4, i5);
+                    }
+                }
+            }
+        }
+    }
+}
+
+template <class T,
+          stdex::extents<>::size_type n1_x,
+          stdex::extents<>::size_type n2_x,
+          stdex::extents<>::size_type n3_x,
+          stdex::extents<>::size_type n4_x,
+          stdex::extents<>::size_type n5_x,
+          stdex::extents<>::size_type n6_x,
+          class Layout_x,
+          class Accessor_x,
+          stdex::extents<>::size_type n1_y,
+          stdex::extents<>::size_type n2_y,
+          stdex::extents<>::size_type n3_y,
+          stdex::extents<>::size_type n4_y,
+          stdex::extents<>::size_type n5_y,
+          stdex::extents<>::size_type n6_y,
+          class Layout_y,
+          class Accessor_y>
+inline void
+copy(stdex::mdspan<T,
+                   stdex::extents<n1_x, n2_x, n3_x, n4_x, n5_x, n6_x>,
+                   Layout_x,
+                   Accessor_x> x,
+     stdex::mdspan<T,
+                   stdex::extents<n1_y, n2_y, n3_y, n4_y, n5_x, n6_x>,
+                   Layout_y,
+                   Accessor_y> y)
+{
+    static_assert(x.static_extent(0) == y.static_extent(0));
+    static_assert(x.static_extent(1) == y.static_extent(1));
+    static_assert(x.static_extent(2) == y.static_extent(2));
+    static_assert(x.static_extent(3) == y.static_extent(3));
+    static_assert(x.static_extent(4) == y.static_extent(4));
+    static_assert(x.static_extent(5) == y.static_extent(5));
+
+    using size_type = stdex::extents<>::size_type;
+
+    for (size_type i1 = 0; i1 < y.extent(0); ++i1) {
+        for (size_type i2 = 0; i2 < y.extent(1); ++i2) {
+            for (size_type i3 = 0; i3 < y.extent(2); ++i3) {
+                for (size_type i4 = 0; i4 < y.extent(3); ++i4) {
+                    for (size_type i5 = 0; i5 < y.extent(4); ++i5) {
+                        for (size_type i6 = 0; i6 < y.extent(5); ++i6) {
+                            y(i1, i2, i3, i4, i5, i6) =
+                                x(i1, i2, i3, i4, i5, i6);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+template <class T,
+          stdex::extents<>::size_type n1_x,
+          stdex::extents<>::size_type n2_x,
+          stdex::extents<>::size_type n3_x,
+          stdex::extents<>::size_type n4_x,
+          stdex::extents<>::size_type n5_x,
+          stdex::extents<>::size_type n6_x,
+          stdex::extents<>::size_type n7_x,
+          class Layout_x,
+          class Accessor_x,
+          stdex::extents<>::size_type n1_y,
+          stdex::extents<>::size_type n2_y,
+          stdex::extents<>::size_type n3_y,
+          stdex::extents<>::size_type n4_y,
+          stdex::extents<>::size_type n5_y,
+          stdex::extents<>::size_type n6_y,
+          stdex::extents<>::size_type n7_y,
+          class Layout_y,
+          class Accessor_y>
+inline void
+copy(stdex::mdspan<T,
+                   stdex::extents<n1_x, n2_x, n3_x, n4_x, n5_x, n6_x, n7_x>,
+                   Layout_x,
+                   Accessor_x> x,
+     stdex::mdspan<T,
+                   stdex::extents<n1_y, n2_y, n3_y, n4_y, n5_x, n6_x, n7_x>,
+                   Layout_y,
+                   Accessor_y> y)
+{
+    static_assert(x.static_extent(0) == y.static_extent(0));
+    static_assert(x.static_extent(1) == y.static_extent(1));
+    static_assert(x.static_extent(2) == y.static_extent(2));
+    static_assert(x.static_extent(3) == y.static_extent(3));
+    static_assert(x.static_extent(4) == y.static_extent(4));
+    static_assert(x.static_extent(5) == y.static_extent(5));
+    static_assert(x.static_extent(6) == y.static_extent(6));
+
+    using size_type = stdex::extents<>::size_type;
+
+    for (size_type i1 = 0; i1 < y.extent(0); ++i1) {
+        for (size_type i2 = 0; i2 < y.extent(1); ++i2) {
+            for (size_type i3 = 0; i3 < y.extent(2); ++i3) {
+                for (size_type i4 = 0; i4 < y.extent(3); ++i4) {
+                    for (size_type i5 = 0; i5 < y.extent(4); ++i5) {
+                        for (size_type i6 = 0; i6 < y.extent(5); ++i6) {
+                            for (size_type i7 = 0; i7 < y.extent(6); ++i7) {
+                                y(i1, i2, i3, i4, i5, i6, i7) =
+                                    x(i1, i2, i3, i4, i5, i6, i7);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 } // namespace Scilib
 
 #endif // SCILIB_MDARRAY_COPY_H
