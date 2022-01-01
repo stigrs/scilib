c0411734d92f544c0fbd2ac7e05e50077fff8241
Stig Rune Sellevag <stigrs@gmail.com>
2022-01-01 17:18:00 +0100

    Add TestConstCorrectness

--- tests/test_array3d.cpp
+++ tests/test_array3d.cpp
@@ -7,6 +7,12 @@
 #include <scilib/mdarray.h>
 #include <gtest/gtest.h>
 
+void beast(const Sci::Array3D<int>& a)
+{
+    Sci::Array3D<int> b = Sci::slice(stdex::full_extent, stdex::full_extent, stdex::full_extent);
+    b(1, 1, 1) = 666;
+}
+
 TEST(TestMDArray, TestArray3D)
 {
     Sci::Array3D<int> a(3, 4, 5);
@@ -19,4 +25,21 @@ TEST(TestMDArray, TestArray3D)
             }
         }
     }
+}
+
+TEST(TestMDArray, TestConstCorrectness)
+{
+    Sci::Array3D<int> a(5, 5, 5);
+    Sci::Array3D<int> b(5, 5, 5);
+
+    for (std::size_t i = 0; i < a.extent(0); ++i) {
+        for (std::size_t j = 0; j < a.extent(1); ++j) {
+            for (std::size_t k = 0; k < a.extent(2); ++k) {
+                a(i, j, k) = i + j + k;
+                b(i, j, k) = i + j + k;
+            }
+        }
+    }
+    beast(a);
+    EXPECT_EQ(a, b);
 }
