ef81c268ac6f76e26f2c04ac2c679e316c079cb8
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2023-07-26 17:57:15 +0200

    bug fix

--- bench/bench_finite_difference.cpp
+++ bench/bench_finite_difference.cpp
@@ -27,16 +27,16 @@ T finite_difference_impl(Sci::StaticMatrix<T, num, num>& u)
 
     auto u_old = u;
 
-    auto u_old1 = slice(u_old, seq(0, num - 3), seq(1, num - 2));
-    auto u_old2 = slice(u_old, seq(2, num - 1), seq(1, num - 2));
-    auto u_old3 = slice(u_old, seq(1, num - 2), seq(0, num - 3));
-    auto u_old4 = slice(u_old, seq(1, num - 2), seq(2, num - 1));
-    auto u_old5 = slice(u_old, seq(0, num - 3), seq(0, num - 3));
-    auto u_old6 = slice(u_old, seq(0, num - 3), seq(2, num - 1));
-    auto u_old7 = slice(u_old, seq(2, num - 1), seq(0, num - 3));
-    auto u_old8 = slice(u_old, seq(2, num - 1), seq(2, num - 1));
+    auto u_old1 = slice(u_old, seq(0, num - 2), seq(1, num - 1));
+    auto u_old2 = slice(u_old, seq(2, num), seq(1, num - 1));
+    auto u_old3 = slice(u_old, seq(1, num - 1), seq(0, num - 2));
+    auto u_old4 = slice(u_old, seq(1, num - 1), seq(2, num));
+    auto u_old5 = slice(u_old, seq(0, num - 2), seq(0, num - 2));
+    auto u_old6 = slice(u_old, seq(0, num - 2), seq(2, num));
+    auto u_old7 = slice(u_old, seq(2, num), seq(0, num - 2));
+    auto u_old8 = slice(u_old, seq(2, num), seq(2, num));
 
-    auto u_span = slice(u, seq(1, num - 2), seq(1, num - 2));
+    auto u_span = slice(u, seq(1, num - 1), seq(1, num - 1));
 
     for (index_type i = 0; i < u_span.extent(0); ++i) {
         for (index_type j = 0; j < u_span.extent(1); ++j) {
